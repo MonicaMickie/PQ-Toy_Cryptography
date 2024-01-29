3cb9567cd7f432b76281566c9059f9b0538c5129
Monica 1900199 <104925298+MonicaMickie@users.noreply.github.com>
2024-01-29 19:02:10 +0200

    Update toy.c

--- toy.c
+++ toy.c
@@ -5,23 +5,28 @@
 
 #define NEG(X) (TK_Q - (X))
 
+// fill array with small random var
 static void toy_fill_small(short *buf, int n) {
     for (int k = 0; k < n; ++k) {
+        // small Binomial distribution using Hamming weight
         short val = rand() & 0xF;
-        val = (val >> 1 & 1) - (val & 1); // small Binomial distribution using Hamming weight
+        val = (val >> 1 & 1) - (val & 1); 
         if (val < 0)
             val += TK_Q;
         buf[k] = val;
     }
 }
 
+// perform polynomial multiplication naively
 static void toy_polmul_naive(short *dst, const short *a, const short *b, int add) {
+    // Naive polynomial multiplication for elements in A and t matrices
     dst[0] = ((dst[0] & -add) + a[0] * b[0] + NEG(a[3]) * b[1] + NEG(a[2]) * b[2] + NEG(a[1]) * b[3]) % TK_Q;
     dst[1] = ((dst[1] & -add) + a[1] * b[0] + a[0] * b[1] + NEG(a[3]) * b[2] + NEG(a[2]) * b[3]) % TK_Q;
     dst[2] = ((dst[2] & -add) + a[2] * b[0] + a[1] * b[1] + a[0] * b[2] + NEG(a[3]) * b[3]) % TK_Q;
     dst[3] = ((dst[3] & -add) + a[3] * b[0] + a[2] * b[1] + a[1] * b[2] + a[0] * b[3]) % TK_Q;
 }
 
+// perform matrix-vector multipication
 static void toy_mulmv(short *dst, const short *mat, const short *vec) {
     memset(dst, 0, TK_K * TK_N * sizeof(short));
     for (int kv = 0, idx = 0; kv < TK_K * TK_N; kv += TK_N) {
@@ -31,6 +36,7 @@ static void toy_mulmv(short *dst, const short *mat, const short *vec) {
     }
 }
 
+// perform matrix transpose times vector multiplication
 static void toy_mulmTv(short *dst, const short *mat, const short *vec) {
     memset(dst, 0, TK_K * TK_N * sizeof(short));
     for (int kv = 0; kv < TK_K * TK_N; kv += TK_N) {
@@ -41,6 +47,7 @@ static void toy_mulmTv(short *dst, const short *mat, const short *vec) {
     }
 }
 
+// compute dot product of 2 vectors
 static void toy_dot(short *dst, const short *v1, const short *v2) {
     memset(dst, 0, TK_N * sizeof(short));
     for (int k = 0; k < TK_K * TK_N; k += TK_N) {
@@ -48,6 +55,7 @@ static void toy_dot(short *dst, const short *v1, const short *v2) {
     }
 }
 
+// add 2 vectors with optional negation of the second
 static void toy_add(short *dst, const short *v1, const short *v2, int count, int v2_neg) {
     for (int k = 0; k < count; ++k) {
         short val = v2[k];
@@ -57,49 +65,43 @@ static void toy_add(short *dst, const short *v1, const short *v2, int count, int
     }
 }
 
+// generate random elements for toy encryption
 void toy_gen(short *A, short *t, short *s) {
     short e[TK_K * TK_N];
     for (int k = 0; k < TK_K * TK_K * TK_N; ++k)
         A[k] = rand() % TK_Q;
     toy_fill_small(s, TK_K * TK_N);
     toy_fill_small(e, TK_K * TK_N);
-
-    toy_mulmv(t, A, s); // t = A.s + e
+    toy_mulmv(t, A, s);
     toy_add(t, t, e, TK_K * TK_N, 0);
 }
 
+// encrypt plaintext
 void toy_enc(const short *A, const short *t, int plain, short *u, short *v) {
     short r[TK_K * TK_N], el[TK_K * TK_N], e2[TK_N];
     toy_fill_small(r, TK_K * TK_N);
     toy_fill_small(el, TK_K * TK_N);
     toy_fill_small(e2, TK_N);
-
-    toy_mulmTv(u, A, r); // u = A^T . r + el
+    toy_mulmTv(u, A, r);
     toy_add(u, u, el, TK_K * TK_N, 0);
-
-    toy_dot(v, t, r); // v = t^T . r + e2 + plain * q/2
+    toy_dot(v, t, r);
     toy_add(v, v, e2, TK_N, 0);
-
     for (int k = 0; k < TK_N; ++k)
         v[k] = (v[k] + ((TK_Q >> 1) & -(plain >> k & 1))) % TK_Q;
 }
 
+// decrypt ciphertext and check against secret key
 int toy_dec(const short *s, const short *u, const short *v) {
     short p[TK_N], plain;
     toy_dot(p, s, u);
     toy_add(p, v, p, TK_N, 1);
-
     plain = 0;
-
     for (int k = 0; k < TK_N; ++k) {
         int val = p[k];
-
         if (val > TK_Q / 2)
             val -= TK_Q;
-
         int bit = abs(val) > TK_Q / 4;
         plain |= bit << k;
     }
-
     return plain;
 }
